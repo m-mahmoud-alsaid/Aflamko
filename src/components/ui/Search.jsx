3862b7046384fb77608c8bc1f59be4b36d4d70da
Mohamed Mahmoud <m.mahmoud.alsaid.official@gmail.com>
2026-05-22 07:01:50 +0300

    Fix Search Bar

--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -4,7 +4,7 @@ function SearchBar({ handleSearch }) {
 
     return (
         <label className='text-secondary-text bg-[#0f1113] rounded-4xl relative pt-2 pb-2 pl-4 border-3 border-border focus-within:border-primary focus-within:text-primary duration-300'>
-            <Search className='absolute right-4 ' />
+            <Search className='absolute right-4 hidden lg:block' />
             <input
                 className='w-full h-5 outline-none text-primary-text focus:text-primary focus:placeholder:text-primary duration-300'
                 placeholder='Search movies...'
